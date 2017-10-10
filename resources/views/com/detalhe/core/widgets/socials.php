78d6a249395610cc1b8a4a2a3e057145b520fc15
yamilelias <yamileliassoto@gmail.com>
2017-10-10 14:03:55 -0600

    Edited socials widget view adding icons

---
+++ resources/views/com/detalhe/core/widgets/socials.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Author: yamilelias
+ * Author URI: <yamileliassoto@gmail.com>
+ * Date: 10/10/17
+ * Time: 12:44 PM
+ */
+?>
+
+<div class="social-media-container">
+    <div class="facebook icon"><i class="icon ion-social-facebook"></i></div>
+    <div class="instagram icon"><i class="icon ion-social-instagram"></i></div>
+</div>
