1829c9dff6565f7e3317b8ea70aa2bbf6b8694a6
yamilelias <yamileliassoto@gmail.com>
2017-11-03 13:37:02 -0700

    First implementation for brands hover effect

--- resources/views/com/detalhe/core/brands/preview.php
+++ resources/views/com/detalhe/core/brands/preview.php
@@ -10,7 +10,12 @@
 
 <div class="no-padding-sides col-lg-6 col-md-6 col-xs-6">
     <div class="item">
-        <a href="<?php echo get_site_url() . '/product-category/' . $brand_slug ?>">
+        <a href="<?php echo get_site_url() . '/product-category/' . $brand_slug ?>" class="brand-link">
+            <div class="caption">
+                <div class="caption-content">
+                    <i class="fa fa-search-plus fa-3x"></i>
+                </div>
+            </div>
             <?php echo $image; ?>
         </a>
     </div>
